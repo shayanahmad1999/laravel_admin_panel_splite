b3ce896c64164a899aaa698186ae4261f79603dc
shayanahmad1999 <shayanahmad235@gmail.com>
2024-02-12 17:01:12 +0500

    remove head portion

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,31 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-
-		<meta charset="UTF-8">
-		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
-		<meta content="Splite - a responsive, flat and full featured admin template" name="description">
-	    <meta content="Spruko Technologies Private Limited" name="author">
-		<meta name="keywords" content="bootstrap admin template,bootstrap dashboard,dashboard template,bootstrap dashboard,admin dashboard,bootstrap admin,html admin template,html dashboard template,bootstrap admin dashboard,themeforest admin template,admin panel template,bootstrap 4 admin template,template admin bootstrap 4,bootstrap dashboard template,dashboard design template">
-		<link rel="icon" href="{{asset('assets/img/brand/favicon.ico')}}" type="image/x-icon"/>
-		<link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/brand/favicon.ico')}}" />
-		<title>Splite-Admin Dashboard</title>
-
-
-		<!--Bootstrap.min css-->
-		<link rel="stylesheet" href="{{asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}">
-
-		<!--Style css-->
-		<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-
-		<!--Icons css-->
-		<link rel="stylesheet" href="{{asset('assets/css/icons.css')}}">
-
-
-		<!--Sidemenu css-->
-		<link rel="stylesheet" href="{{asset('assets/css/sidemenu.css')}}">
-
-	</head>
 
 	<body class="app ">
 
@@ -579,7 +553,7 @@
 							</div>
 						</div>
 						<!--row closed-->
-                        
+
                         <!--row open-->
 						<div class="row">
 							<div class="col-12 col-sm-12">
