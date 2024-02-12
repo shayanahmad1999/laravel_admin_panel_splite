96a2bbc05f24104bf1e3d1e21ecc4b2e366f72ef
shayanahmad1999 <shayanahmad235@gmail.com>
2024-02-12 16:59:38 +0500

    deleted sm js files

--- public/assets/js/chattype.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$("#addClass").on("click", function (e) {
-		$('#qnimate').addClass('popup-box-on');
-	});
-
-	$("#removeClass").on("click", function (e) {
-		$('#qnimate').removeClass('popup-box-on');
-	});
-})
