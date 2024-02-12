1c6009a87a9f091b4eee23016d55f8366e136815
shayanahmad1999 <shayanahmad235@gmail.com>
2024-02-12 17:04:26 +0500

    remove footer file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -380,23 +380,5 @@
 		</div>
 		<!--app closed-->
 
-		<!-- Back to top -->
-		<a href="#top" id="back-to-top" ><i class="fa fa-angle-up"></i></a>
-
-		<!-- Popup-chat -->
-		<a href="#" id="addClass"><i class="ti-comment"></i></a>
-
-		<!--Jquery.min js-->
-		<script src="{{asset('assets/js/jquery.min.js')}}"></script>
-
-
-		<!--Bootstrap.min js-->
-		<script src="{{asset('assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
-
-		<!--Sidemenu js-->
-		<script src="{{asset('assets/plugins/toggle-menu/sidemenu.js')}}"></script>
-
-		<!--Scripts js-->
-		<script src="{{asset('assets/js/scripts.js')}}"></script>
 	</body>
 </html>
